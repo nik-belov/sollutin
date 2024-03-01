4b56229d99fc71b7ba074ae2496387c86e78af15
Nikita Belov <nick.belov@outlook.com>
2024-03-01 05:36:19 +0300

    fix linters and apos

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,17 +1,22 @@
-import { ReactNode } from "react"
-import { cn } from "~/lib/utils"
+import type { ReactNode } from "react";
+import { cn } from "~/lib/utils";
 
 const MaxWidthWrapper = ({
-    className,
-    children
+  className,
+  children,
 }: {
-    className?: string
-    children: ReactNode
+  className?: string;
+  children: ReactNode;
 }) => {
-    return (
-        <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>
-        {children}
-        </div>
-    )
-}
-export default MaxWidthWrapper
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+export default MaxWidthWrapper;
