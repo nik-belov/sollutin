1dac185cb44d0764c34dd7f2f3625bc6409dcc08
Maksim Filippov <mdfilippov@edu.hse.ru>
2024-03-01 06:38:35 +0300

    new name and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           </div> */}
           <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
             Elevate your online school with{" "}
-            <span className="text-blue-600">AI-powered</span> tutor.
+            <span className="text-green-600">AI-powered</span> tutor.
           </h1>
           <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
             <span className="text-">Sollutin</span> allows you to decrease costs
