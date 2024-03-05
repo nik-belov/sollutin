b402c67794c36d7e79ed53d55c1e707e03478ce0
Nikita Belov <nick.belov@outlook.com>
2024-03-05 13:25:29 +0300

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { Inter } from "next/font/google";
 
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <TRPCReactProvider>{children}</TRPCReactProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
